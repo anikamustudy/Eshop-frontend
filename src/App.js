fce3b0efde5fcc3b8349a8ec3deb1324b736d2c6
Rudrakshya108 <anikamustudy@gmail.com>
2024-08-24 15:05:13 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import About from "./pages/About";
+import Services from "./pages/ServicesPage";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductPage from "./product/ProductPage";
+import ShopPage from "./product/ShopPage";
+import ServicesPage from "./pages/ServicesPage";
+import Homepage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/shoppage" element={<ShopPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
